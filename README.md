14b20697f0ace101354e4ccaa1fc74b99783939a
836948082 <836948082@qq.com>
2017-01-14 11:26:49 +0800

    TwinklingRefreshLayout(带越界回弹刷新、加载控件)

---
+++ README.md
@@ -0,0 +1,4 @@
+# TwinklingRefreshLayout
+# 支持下拉刷新和上拉加载的RefreshLayout,自带越界回弹效果，支持RecyclerView,AbsListView,ScrollView,WebView
+
+https://github.com/lcodecorex/TwinklingRefreshLayout
---
+++ app/src/main/java/com/runtai/twinklingrefreshlayout/TkApplication.java
@@ -0,0 +1,27 @@
+package com.runtai.twinklingrefreshlayout;
+
+import android.app.Application;
+import android.os.StrictMode;
+
+import static android.os.Build.VERSION.SDK_INT;
+import static android.os.Build.VERSION_CODES.GINGERBREAD;
+
+public class TkApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        enabledStrictMode();
+    }
+
+    private void enabledStrictMode() {
+        if (SDK_INT >= GINGERBREAD) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder() //
+                    .detectAll() //
+                    .penaltyLog() //
+                    .penaltyDeath() //
+                    .build());
+        }
+    }
+
+}
